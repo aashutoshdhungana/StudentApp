f2bb74b43191aaa010995dfe9f84ca83fb6cc800
aashutoshdhungana <dhunganayt1205@gmail.com>
2021-12-11 21:40:26 +0545

    focus on invalid input

--- wwwroot/js/site.js
+++ wwwroot/js/site.js
@@ -7,34 +7,42 @@ function formValidation(id) {
         if (form['FirstName'].value === "") {
             console.log("You surely have a first name");
             document.getElementById('FirstNameVal').innerHTML = 'First Name cannot be empty';
+            form['FirstName'].focus();
             return false;
         }
         if (form['LastName'].value === "") {
             document.getElementById('LastNameVal').innerHTML = "Last Name cannot be empty";
+            form['LastName'].focus();
             return false;
         }
         if (form['MiddleName'].value.length > 1) {
             document.getElementById('MiddleNameVal').innerHTML = "Middle Name should be a single character";
+            form['MiddleName'].focus();
             return false;
         }
         if (form['Email'].value === "") {
             console.log("Even dinosaurs have Email");
             document.getElementById('EmailVal').innerHTML = "Email cannot be empty";
+            form['Email'].focus();
+
             return false;
         }
 
         if (!emailValidation(form['Email'].value)) {
             document.getElementById('EmailVal').innerHTML = "Enter a valid email";
+            form['Email'].focus();
             return false;
         }
 
         if (form['PhoneNumber'].value === "") {
             document.getElementById('PhoneNumberVal').innerHTML = "Phone number cannot be empty";
+            form['PhoneNumber'].focus();
             return false;
         }
 
         if (!phoneNumberValidation(form['PhoneNumber'].value)) {
             document.getElementById('PhoneNumberVal').innerHTML = "Enter a valid mobile number";
+            form['PhoneNumber'].focus();
             return false;
         }
         return true;
